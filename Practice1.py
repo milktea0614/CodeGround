bcf815e2893422bb8524edd298fe28d9fbf34ca4
milktea0614 <milktea0614@naver.com>
2022-02-20 22:45:52 +0900

    Add Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,22 @@
+import sys
+
+"""
+https://www.codeground.org/practice/practiceProblemViewNew
+"""
+
+inf = sys.stdin
+T = inf.readline();
+for t in range(0, int(T)):
+    Answer = 0;
+    #############################################################################################
+    n_num = int(inf.readline())
+    n_list = inf.readline().split()
+    for j in range(0,n_num):
+        temp = int(n_list[j])
+        Answer = Answer ^ temp
+        j+=1;
+    #############################################################################################
+    # Print the answer to standard output(screen).
+    print('Case #%d' % (int(t) + 1))
+    print(Answer)
+inf.close()
